c90f6311d2b4e127c126a4781078c18f2d86314d
zhanghaibin <zhanghaibin@jikexueyuan.com>
2015-05-15 11:23:37 +0800

    提交修改

--- code/js/createjob.js
+++ code/js/createjob.js
@@ -1,14 +1,39 @@
+var editor = new Simditor({
+	textarea: $('#editor'),
+	placeholder: '这里输入职位描述(最多1500字符)',
+	upload: false,
+	tabIndent: true,
+	toolbarFloat: false,
+	toolbarFloatOffset: 0,
+	toolbarHidden: false,
+	pasteImage: false,
+	toolbar: [
+		'bold',
+		'italic',
+		'underline',
+		'color',
+		'ol',
+		'ul',
+		'hr',
+		'indent',
+		'outdent'
+	]
+});
 var createJob = {
 	init: function() {
 		this.bindEle();
 	},
 	bindEle: function() {
 		$('#jobSubmit').bind("click", this.submitCheck);
+		editor.on("focus", function() {
+			$('#editor').parents('.item-cont').find('.note.errTxt').remove();
+		})
 	},
 	submitCheck: function() {
 		var $name = $('#jobName'),
 			$laborage = $('#laborage'),
 			$location = $('#location');
+		var editorLen = editor.getValue();
 
 		$name.submitEmptyCheck({
 			note: "请输入职位名称"
@@ -18,13 +43,18 @@ var createJob = {
 		});
 		$location.submitEmptyCheck({
 			note: "请输入工作地点",
-			callback: function() {
-				$('#formJob').submit();
-			}
 		});
+		
+		$('#editor').parents('.item-cont').find('.note.errTxt').remove();
+		if (!editorLen) {
+			$('#editor').parents('.item-cont').append('<div class="note errTxt">请输入职位描述</div>');
+		} else {
+			$('#formJob').submit();
+		}
 	}
 }
 
 $(function() {
+
 	createJob.init();
 });
